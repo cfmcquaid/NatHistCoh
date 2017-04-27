280988b78bb7233c21a447c613427a6fe661bea5
cfmcquaid <finn.mcquaid@lshtm.ac.uk>
2017-04-27 16:38:31 +0100

    Including a more elegant way of calculating incidence, cumulative as well as point

--- NatHistCohort.R
+++ NatHistCohort.R
@@ -5,7 +5,7 @@
 #         ^     ^     ^     ^          #
 #      -> Q <-> S <-> C <-> Y -> M     # Q - Quiescent, S - Subclinical, C - Minimal (C+S-), Y - Disseminated (C+S+, symptomatic), M - Death (due to disease)
 #     /   ^     ^                      #
-#    E    |     |                      # E - Recently Infected
+#    E    |     |                      # E - Recently Infected, T - Cumulative incidence
 #     \   v     v                      #
 #      -> K <-> Z                      # K - Quiescent (slow stream), Z- Subclinical (slow stream),
 #         v     v                      #
@@ -24,12 +24,12 @@ setwd("C:/Users/cfmcquaid/Simulations")
 library("reshape2"); library("deSolve"); library("ggplot2"); library("plyr"); #source("Completed/theme_dark.R")
 # Parameter sets: Average (), Wax (regression), Slow (slow), Full (regression, slow)
 # Ax - rate from compartment A to compartment X, Omega - background mortality
-paramA <- c(Eq=1.00, Ek=0.00, Qs=0.03, Qk=0.00, Kz=0.00, Kq=0.00, Sc=0.22, Sq=0.00, Sz=0.00, Zk=0.00, Zs=0.00, Cy=0.22, Cs=0.00, Ym=0.25, Yc=0.00, Omega=0.02)
-paramW <- c(Eq=1.00, Ek=0.00, Qs=0.03, Qk=0.00, Kz=0.00, Kq=0.00, Sc=0.50, Sq=0.25, Sz=0.00, Zk=0.00, Zs=0.00, Cy=0.50, Cs=0.25, Ym=0.50, Yc=0.25, Omega=0.02)
-paramS <- c(Eq=0.10, Ek=0.90, Qs=0.50, Qk=0.00, Kz=0.01, Kq=0.00, Sc=0.22, Sq=0.00, Sz=0.00, Zk=0.00, Zs=0.05, Cy=0.22, Cs=0.00, Ym=0.25, Yc=0.00, Omega=0.02)
-paramF <- c(Eq=0.10, Ek=0.90, Qs=0.50, Qk=0.00, Kz=0.01, Kq=0.00, Sc=0.50, Sq=0.25, Sz=0.00, Zk=0.25, Zs=0.05, Cy=0.50, Cs=0.25, Ym=0.50, Yc=0.25, Omega=0.02)
+paramA <- c(Eq=1.00, Ek=0.00, Qs=0.03, Qk=0.00, Kz=0.00, Kq=0.00, Sc=0.22, Sq=0.00, Sz=0.00, Zk=0.00, Zs=0.00, Cy=0.22, Cs=0.00, Ym=0.25, Yc=0.00, Omega=0)
+paramW <- c(Eq=1.00, Ek=0.00, Qs=0.03, Qk=0.00, Kz=0.00, Kq=0.00, Sc=0.50, Sq=0.25, Sz=0.00, Zk=0.00, Zs=0.00, Cy=0.50, Cs=0.25, Ym=0.50, Yc=0.25, Omega=0)
+paramS <- c(Eq=0.10, Ek=0.90, Qs=0.50, Qk=0.00, Kz=0.01, Kq=0.00, Sc=0.22, Sq=0.00, Sz=0.00, Zk=0.00, Zs=0.05, Cy=0.22, Cs=0.00, Ym=0.25, Yc=0.00, Omega=0)
+paramF <- c(Eq=0.10, Ek=0.90, Qs=0.50, Qk=0.00, Kz=0.01, Kq=0.00, Sc=0.50, Sq=0.25, Sz=0.00, Zk=0.25, Zs=0.05, Cy=0.50, Cs=0.25, Ym=0.50, Yc=0.25, Omega=0)
 # Initial states of compartments
-state <- c(E=100000, Q=0, K=0, S=0, Z=0, C=0, Y=0, M=0)
+state <- c(E=100000, Q=0, K=0, S=0, Z=0, C=0, Y=0, M=0, T=0)
 # Timespan for simulation
 times <- seq(0, 25, by = 1)
 # ODEfunction
@@ -44,33 +44,36 @@ regr <- function(t, state, parameters){
    dC <- + Sc*S + Yc*Y - (Cs + Cy + Omega)*C
    dY <- + Cy*C -(Yc + Ym + Omega)*Y
    dM <- + Ym*Y
+   dT <- + Sc*S / (E + Q + K + S + Z + C + Y + M)
    # return the rate of change
-   list(c(dE, dQ, dK, dS, dZ, dC, dY, dM))
+   list(c(dE, dQ, dK, dS, dZ, dC, dY, dM, dT))
   })
 }
 # Calculations
 outA <- ode(y = state, times = times, func = regr, parms = paramA); outW <- ode(y = state, times = times, func = regr, parms = paramW); outS <- ode(y = state, times = times, func = regr, parms = paramS); outF <- ode(y = state, times = times, func = regr, parms = paramF)
 outA <- as.data.frame(outA); outW <- as.data.frame(outW); outS <- as.data.frame(outS); outF <- as.data.frame(outF)
+# Calculating incidence, subtracting the previous year's cumulative incidence from this year's
+outA$inc <- head(c(outA$T,0) - c(0,outA$T),-1); outW$inc <- head(c(outW$T,0) - c(0,outW$T),-1); outS$inc <- head(c(outS$T,0) - c(0,outS$T),-1); outW$inc <- head(c(outW$T,0) - c(0,outW$T),-1); 
 # Removing the first data point at time zero as we only sample after a year
 outA <- outA[-c(1), ]; outW <- outW[-c(1), ]; outS <- outS[-c(1), ]; outF <- outF[-c(1), ]; 
-# Formatting the data for plotting - putting into a melted data.fame, with additional columns for the source matrix and the incidence rate
-outA$rate <- outA$S*paramA["Sc"] / rowSums(outA[2:9]); outW$rate <- outW$S*paramW["Sc"] / rowSums(outW[2:9]); outS$rate <- outS$S*paramS["Sc"] / rowSums(outS[2:9]); outF$rate <- outF$S*paramF["Sc"] / rowSums(outF[2:9]);
+# Formatting the data for plotting - putting into a melted data.fame, with additional columns for the source matrix
 outA <- melt(outA, id.vars = c("time")); outW <- melt(outW, id.vars = c("time")); outS <- melt(outS, id.vars = c("time")); outF <- melt(outF, id.vars = c("time"))
 outA$source <- "A"; outW$source <- "W"; outS$source <- "S"; outF$source <- "F"
 out <- rbind(outA, outW, outS, outF)
 # Plot output
 theme_set(theme_bw())
 ##all scenarios
-# ggplot(out[out$variable %in% c("rate"), ], aes(time, value)) + geom_point(size=2) + labs(x = "Time", y = "Incidence") + facet_grid(source ~ . , scales = "free")
+# ggplot(out[out$variable %in% c("inc"), ], aes(time, value)) + geom_point(size=2) + labs(x = "Time", y = "Incidence") + facet_grid(source ~ . , scales = "free")
 ##slow and slow+regression only (S+F)
 out2 <- subset(out, source=='F' | source == 'S', select=time:source)  
-ggplot(out2[out2$variable %in% c("rate"), ], aes(time, value)) + geom_point(size=2) + labs(x = "Time", y = "Incidence") + facet_grid(source ~ . , scales = "free")
+ggplot(out2[out2$variable %in% c("inc"), ], aes(time, value)) + geom_point(size=2) + labs(x = "Time", y = "Incidence") + facet_grid(source ~ . , scales = "free")
 
 # Fitting parameters and state: proportion of individuals disease or dead (due to TB) after Otime years, using parameter set Osource
-Osource <- "S"; Otime <- 5
-Osize <- out[which(out$source == Osource & out$variable %in% c("Q", "S", "K", "Z",  "C", "Y", "M") & out$time == Otime), ];
-Orate <- out[which(out$source == Osource & out$variable %in% c("rate") & out$time %in% c(1:Otime)), ]
-# Cumulative incidence: total incidence including those that may have regressed
-sum(Orate$value)
+Osource <- "A"; Otime <- 5
+Osize <- out[which(out$source == Osource & out$variable %in% c("E", "Q", "S", "K", "Z",  "C", "Y", "M") & out$time == Otime), ];
+Oinc <- out[which(out$source == Osource & out$variable %in% c("T") & out$time == Otime), ];
 # Point prevalence: total incidence excluding those that may have regressed
 sum(Osize$value[Osize$variable %in% c("C", "Y", "M")]) / sum(Osize$value)
+# Cumulative incidence: total incidence including those that may have regressed
+Oinc$value
+
