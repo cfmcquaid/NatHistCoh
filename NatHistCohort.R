52ffa28981e627a52c5af893d524a13d1d028168
cfmcquaid <finn.mcquaid@lshtm.ac.uk>
2017-05-17 11:46:07 +0100

    Remove cases without a slow stream for now
    Calculating parameter values
    
    Ym is now death + self-cure

--- NatHistCohort.R
+++ NatHistCohort.R
@@ -3,7 +3,7 @@
 # Cohort model of TB including regression and a slow stream
 ########################################
 #         ^     ^     ^     ^          #
-#      -> Q <-> S <-> C <-> Y -> M     # Q - Quiescent, S - Subclinical, C - Minimal (C+S-), Y - Disseminated (C+S+, symptomatic), M - Death (due to disease)
+#      -> Q <-> S <-> C <-> Y -> M     # Q - Quiescent, S - Subclinical, C - Minimal (C+S-), Y - Disseminated (C+S+, symptomatic), M - Death (due to disease) ||OR|| Self-cure
 #     /   ^     ^                      #
 #    E    |     |                      # E - Recently Infected, T - Cumulative incidence (using initial disease-free population)
 #     \   v     v                      #
@@ -11,8 +11,8 @@
 #         v     v                      #
 ########################################
 ##set wd depending on who is coding/playing
-#setwd("C:/Users/cfmcquaid/Simulations")
-setwd("/Users/ReinHouben/Filr/ifolder/Applications (funding and jobs)/2016 - ERC starting grant/Rmodel_nathis/NatHistCoh")
+setwd("C:/Users/cfmcquaid/Simulations")
+#setwd("/Users/ReinHouben/Filr/ifolder/Applications (funding and jobs)/2016 - ERC starting grant/Rmodel_nathis/NatHistCoh")
 
 ### URL for how to upload changes: http://r-bio.github.io/intro-git-rstudio/
 ### to pull in Finn's version - type in shell: git pull upstream master
@@ -35,9 +35,13 @@ setwd("/Users/ReinHouben/Filr/ifolder/Applications (funding and jobs)/2016 - ERC
 library("reshape2"); library("deSolve"); library("ggplot2"); library("plyr");
 # Parameter sets: Average (), Wax (regression), Slow (slow), Full (regression, slow)
 # Ax - rate from compartment A to compartment X, Omega - background mortality
-paramA <- c(Eq=1.00, Qs=0.03, Qk=0.00, Kz=0.00, Kq=0.00, Sc=0.22, Sq=0.00, Sz=0.00, Zk=0.00, Zs=0.00, Cy=0.22, Cs=0.00, Ym=0.25, Yc=0.00, Omega=0)
-paramW <- c(Eq=1.00, Qs=0.03, Qk=0.00, Kz=0.00, Kq=0.00, Sc=0.50, Sq=0.25, Sz=0.00, Zk=0.00, Zs=0.00, Cy=0.50, Cs=0.25, Ym=0.50, Yc=0.25, Omega=0)
-paramS <- c(Eq=0.10, Qs=0.50, Qk=0.00, Kz=0.01, Kq=0.00, Sc=0.22, Sq=0.00, Sz=0.00, Zk=0.00, Zs=0.05, Cy=0.22, Cs=0.00, Ym=0.25, Yc=0.00, Omega=0)
+
+
+# No regression: 1/Cy + 1/Ym = 3, Ym = 0.5 (0.3 death 0.2 self-cure), therefore Cy = 1. NOTE: 70% die, 30% self-cure
+#
+# paramA <- c(Eq=1.00, Qs=0.03, Qk=0.00, Kz=0.00, Kq=0.00, Sc=0.22, Sq=0.00, Sz=0.00, Zk=0.00, Zs=0.00, Cy=0.22, Cs=0.00, Ym=0.25, Yc=0.00, Omega=0)
+# paramW <- c(Eq=1.00, Qs=0.03, Qk=0.00, Kz=0.00, Kq=0.00, Sc=0.50, Sq=0.25, Sz=0.00, Zk=0.00, Zs=0.00, Cy=0.50, Cs=0.25, Ym=0.50, Yc=0.25, Omega=0)
+paramS <- c(Eq=0.10, Qs=0.50, Qk=0.00, Kz=0.01, Kq=0.00, Sc=0.22, Sq=0.00, Sz=0.00, Zk=0.00, Zs=0.05, Cy=1.00, Cs=0.00, Ym=0.50, Yc=0.00, Omega=0)
 paramF <- c(Eq=0.10, Qs=0.50, Qk=0.00, Kz=0.01, Kq=0.00, Sc=0.50, Sq=0.25, Sz=0.00, Zk=0.25, Zs=0.05, Cy=0.50, Cs=0.25, Ym=0.50, Yc=0.25, Omega=0)
 # Initial states of compartments
 state <- c(E=100000, Q=0, K=0, S=0, Z=0, C=0, Y=0, M=0, T=0)
@@ -96,17 +100,17 @@ calc <- function(ts, tb, state, fxn, parameters, source){
   out$source <- source
   return(out)
 }
-outA <- calc(ts = times, tb = timeb, state = state, fxn = regr, parameters = paramA, source = "A")
-outW <- calc(ts = times, tb = timeb, state = state, fxn = regr, parameters = paramW, source = "W")
+# outA <- calc(ts = times, tb = timeb, state = state, fxn = regr, parameters = paramA, source = "A")
+# outW <- calc(ts = times, tb = timeb, state = state, fxn = regr, parameters = paramW, source = "W")
 outS <- calc(ts = times, tb = timeb, state = state, fxn = regr, parameters = paramS, source = "S")
 outF <- calc(ts = times, tb = timeb, state = state, fxn = regr, parameters = paramF, source = "F")
-out <- rbind(outA, outW, outS, outF)
+out <- rbind(outS, outF)
 # Plot output
 theme_set(theme_bw())
 ##all scenarios
-ggplot(out[out$variable %in% c("int"), ], aes(time, value)) + geom_point(size=2) + labs(x = "Time", y = "Incidence") + facet_grid(source ~ . , scales = "free")
+ggplot(out[out$variable %in% c("int"), ], aes(time, value)) + geom_point(size=2) + labs(x = "Time", y = "Incidence") + facet_grid(source ~ . , scales = "fixed")
 ##slow and slow+regression only (S+F)
-out2 <- subset(out, source=='F' | source == 'S', select=time:source)  
+# out2 <- subset(out, source=='F' | source == 'S', select=time:source)  
 #ggplot(out2[out2$variable %in% c("inc"), ], aes(time, value)) + geom_point(size=2) + labs(x = "Time", y = "Incidence") + facet_grid(source ~ . , scales = "free")
 
 # Fitting parameters and state: proportion of individuals diseased after "Otime" years, using parameter set "Osource"
