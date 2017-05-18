c70edc5d6eb5c7278deed49cdaf742d9e969c64c
cfmcquaid <finn.mcquaid@lshtm.ac.uk>
2017-05-18 17:08:52 +0100

    Update after meeting with Hanif A very quick pass at this, I need to check I've done all the equations right!
    Note - incidence and burn period now use Y only (not C)
    
    Changed the infection progression system (movement between Z & Q, no longer between K& Q),
    
    Included self-cure,
    
    Included mortality of smear-ve,
    
    Renamed states
    
    Tidyied up (removed unused code)

--- NatHistCohort.R
+++ NatHistCohort.R
@@ -1,14 +1,20 @@
 # CF McQuaid & RMGJ Houben
-# Start 21/04/2017 ---> current version 26/04/2017
+# Start 21/04/2017 ---> current version after revision with Hanif 18/05/2017
 # Cohort model of TB including regression and a slow stream
 ########################################
-#         ^     ^     ^     ^          #
-#      -> Q <-> S <-> C <-> Y -> M     # Q - Quiescent, S - Subclinical, C - Minimal (C+S-), Y - Disseminated (C+S+, symptomatic), M - Death (due to disease) ||OR|| Self-cure
-#     /   ^     ^                      #
-#    E    |     |                      # E - Recently Infected, T - Cumulative incidence (using initial disease-free population)
-#     \   v     v                      #
-#      -> K <-> Z                      # K - Quiescent (slow stream), Z- Subclinical (slow stream),
-#         v     v                      #
+#                                      #
+#                        M             # M - Death (due to disease)
+#                       ^ ^            #
+#                      /   \           #
+#      -> Q <-> S <-> C <-> Y          # Q - Failed granuloma, S - Subclinical, C - Minimal (C+S-), Y - Disseminated (C+S+, symptomatic)
+#     /    \    ^                      #
+#    E       \  |                      # E - Exposed
+#     \        \v                      #
+#      -> K <-> Z                      # K - Controlled granuloma, Z- Leaky granuloma,
+#         |                            #
+#         v                            #
+#         R                            # R - Sterilized granuloma (self-cure)
+#                                      #
 ########################################
 ##set wd depending on who is coding/playing
 setwd("C:/Users/cfmcquaid/Simulations")
@@ -40,12 +46,10 @@ library("reshape2"); library("deSolve"); library("ggplot2"); library("plyr");
 # No regression: 1/Cy + 1/Ym = 3 years, Ym = 0.5 (0.3 death 0.2 self-cure), therefore Cy = 1. NOTE: 70% die, 30% self-cure
 # Regression: 1/(Yc + Ym) = 0.5 years, Ym = 0.5 (as above), therefore Yc = 1.5.
 # Regression: 1/(Cy + Cs) = 0.5 years. Assume ratio 2:1 for Cy:Cs, therefore Cs = 2/3
-# paramA <- c(Eq=1.00, Qs=0.03, Qk=0.00, Kz=0.00, Kq=0.00, Sc=0.22, Sq=0.00, Sz=0.00, Zk=0.00, Zs=0.00, Cy=0.22, Cs=0.00, Ym=0.25, Yc=0.00, Omega=0)
-# paramW <- c(Eq=1.00, Qs=0.03, Qk=0.00, Kz=0.00, Kq=0.00, Sc=0.50, Sq=0.25, Sz=0.00, Zk=0.00, Zs=0.00, Cy=0.50, Cs=0.25, Ym=0.50, Yc=0.25, Omega=0)
-paramS <- c(Eq=0.10, Qs=1.00, Qk=0.00, Kz=0.01, Kq=0.00, Sc=1.00, Sq=0.00, Sz=0.00, Zk=0.00, Zs=0.01, Cy=1.00, Cs=0.00, Ym=0.50, Yc=0.00, Omega=0)
-paramF <- c(Eq=0.10, Qs=1.00, Qk=0.00, Kz=0.01, Kq=0.00, Sc=1.00, Sq=0.25, Sz=0.00, Zk=0.05, Zs=0.01, Cy=1.32, Cs=0.66, Ym=0.50, Yc=1.50, Omega=0)
+paramS <- c(Eq=0.10, Qs=1.00, Qk=0.00, Kz=0.01, Kq=0.00, Sc=1.00, Sq=0.00, Sz=0.00, Zk=0.00, Zs=0.01, Cy=1.00, Cs=0.00, Cm=0.05, Ym=0.50, Yc=0.00, Omega=0.10)
+paramF <- c(Eq=0.10, Qs=1.00, Qk=0.00, Kz=0.01, Kq=0.00, Sc=1.00, Sq=0.25, Sz=0.00, Zk=0.05, Zs=0.01, Cy=1.32, Cs=0.66, Cm=0.05, Ym=0.50, Yc=1.50, Omega=0.10)
 # Initial states of compartments
-state <- c(E=100000, Q=0, K=0, S=0, Z=0, C=0, Y=0, M=0, T=0)
+state <- c(E=100000, Q=0, K=0, S=0, Z=0, C=0, Y=0, R=0, M=0)
 # Timespan for simulation
 times <- seq(0, 10, by = 1)
 # Timespan for burn
@@ -57,16 +61,16 @@ regr <- function(t, state, parameters){
   with(as.list(c(state, parameters)), {
    # rates of change
    dE <- - E
-   dQ <- + Eq*E + Sq*S + Kq*K - (Qs + Qk + Omega)*Q
+   dQ <- + Eq*E + Sq*S + Kq*K - (Qs + Qk)*Q
    dK <- + (1 - Eq)*E + Zk*Z + Qk*Q - (Kz + Kq + Omega)*K
-   dS <- + Qs*Q + Cs*C + Zs*Z - (Sc + Sq + Sz + Omega)*S
-   dZ <- + Kz*K + Sz*S - (Zk + Zs + Omega)*Z
-   dC <- + Sc*S + Yc*Y - (Cs + Cy + Omega)*C
-   dY <- + Cy*C -(Yc + Ym + Omega)*Y
-   dM <- + Ym*Y
-   dT <- + Sc*S / sum(state[1:5])
+   dS <- + Qs*Q + Cs*C + Zs*Z - (Sc + Sq + Sz)*S
+   dZ <- + Kz*K + Sz*S - (Zk + Zs)*Z
+   dC <- + Sc*S + Yc*Y - (Cs + Cy + Cm)*C
+   dY <- + Cy*C -(Yc + Ym)*Y
+   dR <- + Omega*K
+   dM <- + Ym*Y + Cm*C
    # return the rate of change
-   list(c(dE, dQ, dK, dS, dZ, dC, dY, dM, dT))
+   list(c(dE, dQ, dK, dS, dZ, dC, dY, dR, dM))
   })
 }
 # Burn off the first X years to establish the proportion of individuals in each compartment, removing diseased
@@ -80,29 +84,13 @@ calc <- function(ts, tb, state, fxn, parameters, source){
   stateb <- burn(tb = tb, state = state, fxn = regr, parameters = parameters)
   out <- ode(y = stateb, times = ts, func = fxn, parms = parameters)
   out <- as.data.frame(out)
-  # Calculating incidence
-  out$inc <- parameters["Sc"]*out$S / (out$E + out$Q + out$S + out$K + out$Z)
-  out$inc <- head(c(0,out$inc),-1) # We later remove the first element as it is zero if calculated using C&Y, but will be nonnegative if looking at the rate. Shift the index accordingly
-  # Calculating incidence rate (https://en.wikipedia.org/wiki/Incidence_(epidemiology)), proportion of population that result in cases per year = (total # cases incl mort) / {[(C+Y)_t1-(C+Y)_t0]*(t1+t0)/2 + t1*(E+Q+S+K+Z)_t1}
-  out$rate <- (out$C + out$Y + out$M) / (head(c(out$C,0) + c(out$Y,0) - c(0,out$C) - c(0,out$Y), -1)*head(c(out$time,0) - c(0,out$time), -1) / 2 + (out$E + out$Q + out$S + out$K + out$Z)*out$time)
-  # Calculating the "relative risk" (see Vynnycky & Fine, 1997)
-  # out$risk <- head(c(out$C,0) + c(out$Y,0) - c(0,out$C) - c(0,out$Y), -1)/(out$C[2] + out$Y[2])
-  # out$risk <- parameters["Sc"]*out$S/(parameters["Sc"]*out$S[1])
-  # out$risk <- head(c(0,out$risk),-1) # see above re index shift
   # Calculating the "interval from conversion" (see Styblo 1991 & TSRU progress report 1967)
-  out$int <- parameters["Sc"]*out$S/sum(parameters["Sc"]*out$S)
-  out$int <- head(c(0,out$int),-1) # see above re index shift
-  # Calculating point prevalence
-  out$prev <- (out$C + out$Y) / (out$E + out$Q + out$S + out$K + out$Z + out$C + out$Y)
-  # Removing the first data point at time zero as we only sample after a year
-  out <- out[-c(1), ]
+  out$int <- parameters["Cy"]*out$C/sum(parameters["Cy"]*out$C)
   # Formatting the data for plotting - putting into a melted data.fame, with additional columns for the source matrix
   out <- melt(out, id.vars = c("time"))
   out$source <- source
   return(out)
 }
-# outA <- calc(ts = times, tb = timeb, state = state, fxn = regr, parameters = paramA, source = "A")
-# outW <- calc(ts = times, tb = timeb, state = state, fxn = regr, parameters = paramW, source = "W")
 outS <- calc(ts = times, tb = timeb, state = state, fxn = regr, parameters = paramS, source = "S")
 outF <- calc(ts = times, tb = timeb, state = state, fxn = regr, parameters = paramF, source = "F")
 # Including the data for barplot
@@ -114,17 +102,9 @@ theme_set(theme_bw())
 ##all scenarios
 # ggplot(out[out$variable %in% c("int"), ], aes(time, value)) + geom_point(size=2) + labs(x = "Time", y = "Incidence") + facet_grid(source ~ . , scales = "fixed")
 ggplot(out[out$variable %in% c("int"), ], aes(x = time, y = value, colour = source)) + geom_line(size=2) + labs(x = "Time", y = "Incidence") # + facet_grid(source ~ . , scales = "fixed")
-##slow and slow+regression only (S+F)
-# out2 <- subset(out, source=='F' | source == 'S', select=time:source)  
-#ggplot(out2[out2$variable %in% c("inc"), ], aes(time, value)) + geom_point(size=2) + labs(x = "Time", y = "Incidence") + facet_grid(source ~ . , scales = "free")
 
 # Fitting parameters and state: proportion of individuals diseased after "Otime" years, using parameter set "Osource"
 Osource <- "F"; Otime <- 5
-Ci <- out[which(out$source == Osource & out$variable == "T" & out$time == Otime), ]
-Ic <- out[which(out$source == Osource & out$variable == "inc" & out$time == Otime), ]
-Ra <- out[which(out$source == Osource & out$variable == "rate" & out$time == Otime), ]
-Pp <- out[which(out$source == Osource & out$variable == "prev" & out$time == Otime), ]
-Rr <- out[which(out$source == Osource & out$variable == "risk" & out$time == Otime), ]
 Iv <- out[which(out$source == Osource & out$variable == "int" & out$time == Otime), ]
 
 # # Tornado plot
