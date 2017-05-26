f3617f8e63dee51addf456d0db65866648e042d1
cfmcquaid <finn.mcquaid@lshtm.ac.uk>
2017-05-26 14:45:22 +0100

    Further cleansing of code

--- NatHistCohort.R
+++ NatHistCohort.R
@@ -1,7 +1,7 @@
 # CF McQuaid & RMGJ Houben
 # Start 21/04/2017 ---> current version after code-cleansing 25/05/2017
 # Cohort model of TB including regression and a slow stream
-########################################
+### DIAGRAM ###############################################################################################################
 #                                      #
 #                        M             # M - Death (due to disease)
 #                       ^ ^            #
@@ -15,7 +15,7 @@
 #         v                            #
 #         R                            # R - Sterilized granuloma (self-cure)
 #                                      #
-########################################
+### NOTES ################################################################################################################
 ##set wd depending on who is coding/playing
 setwd("C:/Users/cfmcquaid/Simulations")
 #setwd("/Users/ReinHouben/Filr/ifolder/Applications (funding and jobs)/2016 - ERC starting grant/Rmodel_nathis/NatHistCoh")
@@ -37,10 +37,8 @@ setwd("C:/Users/cfmcquaid/Simulations")
 # OTHER NEXT STEPS - 8th May 2017
 ##  ?can we calculate in model how often (on average) people would come in and out of states over course of say 2 years?
 ##  ?Can we calculate average time in compartment in each step visit? Should be simple competing risk model right?
-##########################################################################################################################
 ### FUNCTIONS ############################################################################################################
-##########################################################################################################################
-# Disease dynamics  ######################################################################################################
+# DISEASE DYNAMICS
 regr <- function(t, state, parameters){
   with(as.list(c(state, parameters)), {
     # rates of change
@@ -57,52 +55,66 @@ regr <- function(t, state, parameters){
     list(c(dE, dK, dR, dZ, dQ, dS, dC, dY, dM))
   })
 }
-# Burn  ##################################################################################################################
-burn <- function(tb, state, fxn, parameters){ # Remove the first X years to establish propn of indivs in each compartment, removing Y & M
+# BURN
+burn <- function(tb, state, fxn, parameters){
   if (tb>0){
+    # Calculate up until end of burn period
     out <- ode(y=state, times=seq(0,tb,by=1), func=fxn, parms=parameters)
+    # Reset initial state removing indivs in Y & M
     stateb <- c(out[tb+1,2], out[tb+1,3], out[tb+1,4], out[tb+1,5], out[tb+1,6], out[tb+1,7], out[tb+1,8], Y=0, M=0)}
   else{stateb <- state}
   return(stateb)
 }
-# Intervention  ##########################################################################################################
-interv <- function(ti, state, fxn, parameters){# Intervention after X years, moves indivs from Y to R
+# INTERVENTION
+interv <- function(ti, state, fxn, parameters){
   if (ti>0){
+    # Calculate up until intervention
     outi <- ode(y=state, times=seq(0,ti,by=1), func=fxn, parms=parameters)
+    # Remove year 0
     outi <- outi[-1,]
+    # Reset initial state moving indivs from Y to R
     statei <- c(outi[ti,2], outi[ti,3], outi[ti,4]+outi[ti,9], outi[ti,5], outi[ti,6], outi[ti,7], outi[ti,8], Y=0, outi[ti,10])}
   else{statei <- state
-  outi <- numeric()}
-  intout <- list("out"=outi, "state"=statei)
+    # If no intervention output empty out matrix & identical state
+    outi <- numeric()}
+    intout <- list("out"=outi, "state"=statei)
   return(intout)
 }
-# Simulate  ##############################################################################################################
-calc <- function(ts, tb, ti, state, fxn, parameters, source){# Run simulation, incorporating  burn period & intervention into disease dynamics
+# SIMULATE
+calc <- function(ts, tb, ti, state, fxn, parameters, source){
+  # Run simulation, incorporating  burn period & intervention into disease dynamics
+  # Calculate initial state after burn
   stateb <- burn(tb=tb, state=state, fxn=regr, parameters=parameters)
+  # Calculate out matrix until intervention and state after intervention
   intout <- interv(ti=ti, state=stateb, fxn=regr, parameters=parameters)
   outi <- as.data.frame(intout$out)
-  out <- ode(y=intout$state, times=seq(0,ts,by=1), func=fxn, parms=parameters)
+  # Calculate after intervention
+  out <- ode(y=intout$state, times=seq(0,ts-ti,by=1), func=fxn, parms=parameters)
   out <- as.data.frame(out)
-  # Removing the first timestep, time zero, and all timesteps after "times"
+  # Removing year 0
   out <- out[-1,]
+  # Update times to account for intervention
   out$time <- out$time+ti
+  # Merge output pre- & post-intervention
   out <- rbind(outi,out)
-  out <- out[1:10,]
-  # Calculating the "interval from conversion" (see Styblo 1991 & TSRU progress report 1967)
+  # Calculate interval from conversion (see Styblo 1991 & TSRU progress report 1967)
   out$int <- parameters["Cy"]*out$C/sum(parameters["Cy"]*out$C)
+  # Calculate prevalence as compartments Y + C
   out$prev <- out$Y+out$C
-  # Formatting the data for plotting - putting into a melted data.fame, with additional columns for the source matrix
+  # Melt data.fame for plotting
   out <- melt(out, id.vars=c("time"))
+  # Include column for source of parameters behind data
   out$source <- source
   return(out)
 }
-# Tornado  ###############################################################################################################
+# TORNADO
 torn <- function(ts, tb, ti, tt, state, fxn, parameters, source){# Calculations for tornado plots
-    # Store data
+    # Storage fir results
     data <- rbind(parameters, parameters)
     rownames(data) <- c('+1%', '-1%')
-    # Compare to default data set
+    # Calculate with default data set
     out <- calc(ts=ts, tb=tb, ti=ti, state=state, fxn=regr, parameters=parameters, source=source)
+    # Extract result
     def <- out[which(out$time==tt & out$variable=="int"),"value"]
     for (i in 1:16){
       # Increasing and decreasing each parameter in turn
@@ -110,8 +122,10 @@ torn <- function(ts, tb, ti, tt, state, fxn, parameters, source){# Calculations 
       parametersL = parameters
       parametersM[i] = parametersM[i] + range*parametersM[i]
       parametersL[i] = parametersL[i] - range*parametersL[i]
+      # Calculating with increased and decreased parameters
       outM <- calc(ts = ts, tb = tb, ti=ti, state = state, fxn = regr, parameters = parametersM, source = source)
       outL <- calc(ts = ts, tb = tb, ti=ti, state = state, fxn = regr, parameters = parametersL, source = source)
+      # Extract results
       outM <- (outM[which(outM$time==tt & outM$variable=="int"),"value"] - def) / def
       outL <- (outL[which(outL$time==tt & outL$variable=="int"),"value"] - def) / def
       data[1, i] <- outM
@@ -119,33 +133,39 @@ torn <- function(ts, tb, ti, tt, state, fxn, parameters, source){# Calculations 
     }
     return(data)
 }
-##########################################################################################################################
 ### CODE #################################################################################################################
-##########################################################################################################################
 library("reshape2"); library("deSolve"); library("ggplot2"); library("plyr"); library("pryr");
-# Parameter values: Ax = rate from compartment A to compartment X
-paramN <- c(Eq=0.10, Kr=0.10, Kz=0.01, Zk=0.00, Zq=0.01, Zs=0.00, Qz=0.00, Qs=1.50, Sz=0.00, Sq=0.00, Sc=1.00, Cs=0.00, Cy=1.00, Cm=0.10, Yc=0.00, Ym=0.50)
-paramR <- c(Eq=0.10, Kr=0.10, Kz=0.02, Zk=0.01, Zq=0.02, Zs=0.00, Qz=0.00, Qs=2.50, Sz=0.01, Sq=1.00, Sc=2.00, Cs=1.00, Cy=2.00, Cm=0.10, Yc=0.10, Ym=0.60)
-# Initial states of compartments
-state <- c(E=100000, K=0, R=0, Z=0, Q=0, S=0, C=0, Y=0, M=0)
-# Timespan: total simulation, burn period, intervention, tornado plot
-time <- list(s=10, b=1, i=2, t=5)
-# Proportion of change in parameters for tornado plot
-range <- 0.01 
-# Calculations with & without regression, with & without intervention
-outN <- calc(ts=time$s, tb=time$b, ti=0, state=state, fxn=regr, parameters=paramN, source="N")
-outR <- calc(ts=time$s, tb=time$b, ti=0, state=state, fxn=regr, parameters=paramR, source="R")
-outNi <- calc(ts=time$s, tb=time$b, ti=time$i, state=state, fxn=regr, parameters=paramN, source="Ni")
-outRi <- calc(ts=time$s, tb=time$b, ti=time$i, state=state, fxn=regr, parameters=paramR, source="Ri")
-# Calculating tornado plot
-dat <- torn(ts=time$s, tb=time$b, ti=time$i, tt=time$t, state=state, fxn=regr, parameters=paramR, source="R")
-# Inclusion of data for barplot
-outD <- data.frame(time=seq(1,time$s,by=1), variable=rep("D",10), value=c(.58,.24,.08,.05,.01,.01,.02,.01,0,0), source=rep("D",10))
-out <- rbind(outN, outR,outNi, outRi, outD)
-##########################################################################################################################
+# PARAMETER VALUES
+  # Ax = rate from compartment A to compartment X
+  # No regression  
+  paramN <- c(Eq=0.10, Kr=0.10, Kz=0.01, Zk=0.00, Zq=0.01, Zs=0.00, Qz=0.00, Qs=1.50, Sz=0.00, Sq=0.00, Sc=1.00, Cs=0.00, Cy=1.00, Cm=0.10, Yc=0.00, Ym=0.50)
+  # Regression
+  paramR <- c(Eq=0.10, Kr=0.10, Kz=0.02, Zk=0.01, Zq=0.02, Zs=0.00, Qz=0.00, Qs=2.50, Sz=0.01, Sq=1.00, Sc=2.00, Cs=1.00, Cy=2.00, Cm=0.10, Yc=0.10, Ym=0.60)
+  # Proportion of change in parameters for tornado plot
+  range <- 0.01
+# INITIAL STATES
+  state <- c(E=100000, K=0, R=0, Z=0, Q=0, S=0, C=0, Y=0, M=0)
+# TIMESPAN
+  # total simulation, burn period, intervention, tornado plot
+  time <- list(s=10, b=1, i=2, t=5)
+# CALCULATIONS
+  # No regression, no intervention
+  outN <- calc(ts=time$s, tb=time$b, ti=0, state=state, fxn=regr, parameters=paramN, source="N")
+  # Regression, no intervention
+  outR <- calc(ts=time$s, tb=time$b, ti=0, state=state, fxn=regr, parameters=paramR, source="R")
+  # No regression, intervention
+  outNi <- calc(ts=time$s, tb=time$b, ti=time$i, state=state, fxn=regr, parameters=paramN, source="Ni")
+  # Regression, intervention
+  outRi <- calc(ts=time$s, tb=time$b, ti=time$i, state=state, fxn=regr, parameters=paramR, source="Ri")
+  # Tornado plot
+  dat <- torn(ts=time$s, tb=time$b, ti=time$i, tt=time$t, state=state, fxn=regr, parameters=paramR, source="R")
+# DATA
+  # Inclusion of data for barplot
+  outD <- data.frame(time=seq(1,time$s,by=1), variable=rep("D",10), value=c(.58,.24,.08,.05,.01,.01,.02,.01,0,0), source=rep("D",10))
+  # Binding all results
+  out <- rbind(outN, outR,outNi, outRi, outD)
 ### PLOTS ################################################################################################################
-##########################################################################################################################
-# Data  ##################################################################################################################
+# DATA
 plot.data <- {ggplot(out[out$variable%in%c("int")&out$source%in%c("R","N"),], aes(x=time,y=value,colour=source)) +
   # Barplot of the data
   geom_bar(data=out[out$variable %in% c("D"),], stat="identity", size=1, fill="white") +
@@ -158,7 +178,7 @@ plot.data <- {ggplot(out[out$variable%in%c("int")&out$source%in%c("R","N"),], ae
   # Legend placement, size, text size
   theme_bw() +
   theme(legend.key.width=unit(2,"cm"), legend.justification=c(1,1), legend.position=c(.99,.99), legend.background=element_rect(size=0.5,linetype="solid",colour ="black"), legend.text=element_text(size=20), legend.title=element_text(size=20), axis.text=element_text(size=15), axis.title=element_text(size=25))}
-# Intervention ###########################################################################################################
+# INTERVENTION
 plot.interv <- {ggplot(out[out$variable%in%c("prev"),], aes(time,value,colour=source,linetype=source)) +
   # Lineplot of model results
   geom_line(size=2) +
@@ -174,7 +194,7 @@ plot.interv <- {ggplot(out[out$variable%in%c("prev"),], aes(time,value,colour=so
   # Legend placement, size, text size
   theme_bw() +
   theme(legend.key.width=unit(2,"cm"), legend.justification=c(1,1), legend.position=c(.99,.99), legend.background=element_rect(size=0.5,linetype="solid",colour="black"), legend.text=element_text(size=20), legend.title=element_text(size=20), axis.text=element_text(size=15), axis.title=element_text(size=25))}
-# Tornado ################################################################################################################
+# TORNADO
 {dev.control('enable')
   x <- seq(-0.01,0.01, length=10)
   ORD = order(abs(dat[2,]-dat[1,]))
